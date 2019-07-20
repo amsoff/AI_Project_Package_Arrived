35383d62b58e9b02abece98457b1d3b4ff3497c4
sofferam <amit.soffer@mail.huji.ac.il>
2019-07-20 16:44:05 +0300

    init rows

--- board.py
+++ board.py
@@ -1,6 +1,10 @@
 import numpy as np
 from enum import Enum
 
+JUMP = 'jump'
+ONE = 1
+THREE = 3
+FIVE = 5
 
 class Nodes_Types(Enum):
     EMPTY = -1
@@ -22,9 +26,9 @@ class Nodes_Types(Enum):
 
 class Board:
 
-    def __init__(self, num_players, starting_point=(0, 1), end_point=(21, 27)):
-        self.board_w = 31
-        self.board_h = 23
+    def __init__(self, num_players, starting_point=(1, 0), end_point=(21, 27)):
+        self.board_w = 23
+        self.board_h = 31
         self.starting_point = starting_point
         self.num_players = num_players
 
@@ -42,8 +46,55 @@ class Board:
         pass
 
     def init_dict(self):
-        for x in range(self.board_w):
-            for y in range(self.board_h):
-                self.transition_dict[(x,y)] = {}
+        self.init_row0()
+        self.init_row1()
+        self.init_row2()
+        self.init_row3()
+        self.init_row4()
+        self.init_row5()
+        self.init_row6()
+        self.init_row7()
+        self.init_row8()
+        self.init_row9()
+        self.init_row10()
+        self.init_row11()
 
-        #
+    def init_row11(self):
+        self.transition_dict[(11, 30)] = {1: [(10, 30), (11, 30)], 3: [(8, 30), (12, 28)],
+                                          5: [(6, 30), (10, 28), (12, 26)], JUMP: []}
+
+    def init_row10(self):
+        self.transition_dict[(10, 30)] = {1: [(11, 30), (9, 30)], 3: [(7, 30), (12, 29)],
+                                          5: [(5, 30), (11, 28), (12, 27)], JUMP: []}
+
+    def init_row9(self):
+        self.transition_dict[(9, 30)] = {1: [(10, 30), (11, 30)], 3: [(12, 30), (6, 30)], 5: [(5, 29), (12, 28)],
+                                         JUMP: []}
+
+    def init_row8(self):
+        self.transition_dict[(8, 30)] = {1: [(9, 30), (7, 30)], 3: [(12, 30), (6, 30)], 5: [(5, 29), (12, 28)],
+                                         JUMP: []}
+
+    def init_row7(self):
+        pass
+
+    def init_row6(self):
+        pass
+
+    def init_row5(self):
+        pass
+
+    def init_row4(self):
+        pass
+
+    def init_row3(self):
+        pass
+
+    def init_row2(self):
+        pass
+
+    def init_row1(self):
+        pass
+
+    def init_row0(self):
+        pass
