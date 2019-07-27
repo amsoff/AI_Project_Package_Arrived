ab91367d242338522c86f4115ef5004b3c167938
reut <reut.rabenou@mail.huji.ac.il>
2019-07-27 14:40:06 +0300

    changes to player fields

--- player.py
+++ player.py
@@ -1,24 +1,38 @@
 from dice import dice
+import board
+from Certificates import Certificate
+
+GRANDMA = 0
+INTEGRITY = 1
+BIRTH = 2
+ID = 3
+RABIES = 4
+PASSPORT = 5
+MILITARY = 6
+TAX = 7
+PORT = 8
+GLASSES = 9
+HAT = 10
+HAIRCUT = 11
+PACKAGE = 12
+
+
+board_game = board.Board(1).transition_dict
+come_back_spots = [tile for tile in board_game if (board.NEED in board_game[tile] and (Certificate.HAT not in board_game[tile][board.NEED] or Certificate.GLASSES not in board_game[tile][board.NEED])) or board.SURPRISE in board_game[tile] or board.BALANCE in board_game[tile]]
+payment_spots = [tile for tile in board_game if board.BALANCE in board_game[tile] or board.SURPRISE in board_game[tile]]
+
 class Player:
     money = 1500
-    has_grandmas_marriage_certificate = False
-    has_integrity_certificate = False
-    has_birth_certificate = False
-    has_id = False
-    has_rabies_certificate = False
-    has_passport_photo = False
-    has_military_pad = False
-    has_tax_payment_authorization = False
-    has_entrance_to_port = False
-    has_glasses = False
-    has_hat = False
-    has_package = False
-    package_cost = 0
-    come_back_spots = []
     cell = (0,0)
-    dice = dice()
+    has = [False] * 13
+    not_needs = []
     dice_value = 0
+    come_back_spots = []
+    need_pay_spots = []
     stops_left = 0
+    package_cost = 0
+    dice = dice()
+
 
 
     def pay(self, cost, is_surprise = False):
