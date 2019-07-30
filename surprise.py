9602f84084ded3ebc73e42930cb4c7c51ef3585e
Nitzan Wagner <t-niwagn@microsoft.com>
2019-07-30 16:27:12 +0300

    Merge branch 'master' of https://github.com/amsoff/AI_Project_Package_Arrived
    # Conflicts:
    #	board.py

--- surprise.py
+++ surprise.py
@@ -5,7 +5,7 @@ from statistics import mean
 class Surprise:
     surprises = [-300, -200, -100, 0, 200, 300]
     optimistic_expected_surprise = max(surprises)
-    mean_expected_surprise = mean(surprises)
+    mean_expected_surprise = -100
 
     def get_surprise(self):
         surprise = random.choice(self.surprises)
