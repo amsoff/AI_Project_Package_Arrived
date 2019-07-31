e47a1a94eb15763058c155e4eb784d7d5ad7695d
sofferam <amit.soffer@mail.huji.ac.il>
2019-07-31 12:36:59 +0300

    prints

--- graphplan/search.py
+++ graphplan/search.py
@@ -106,19 +106,18 @@ def general_search(problem, fringe):
     while not fringe.isEmpty():
         i = 1
         curr = fringe.pop()
-        if i % 1 == 0:
-            print("round %d" % i)
-
+        # if i % 1 == 0:
+            # print("round %d" % i)
         if problem.is_goal_state(curr.get_node()):
             return get_path(curr)
 
         elif curr.get_node() not in closed:
             successors = problem.get_successors(curr.get_node())
-            if i % 1 == 0:
-                print("Num_successors = %d" % len(successors))
+            # if i % 1 == 0:
+                # print("Num_successors = %d" % len(successors))
             for i in range(len(successors)):
                 fringe.push(PQItem((successors[i][0], successors[i][1], successors[i][2] + curr.get_cost(), curr)))
-                print(successors[i][1].name)
+                # print(successors[i][1].name)
             closed.add(curr.get_node())
         i += 1
     return []
