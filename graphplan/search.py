60ef950abffc7fbf15b4813e009ece0b3baa5617
sofferam <amit.soffer@mail.huji.ac.il>
2019-08-07 16:07:09 +0300

    bla

--- graphplan/search.py
+++ graphplan/search.py
@@ -75,23 +75,19 @@ class SearchProblem:
         util.raiseNotDefined()
 
 
+def get_path(curr_node):
+    """
 
-def get_past_path(cur_node):
+    :param curr_node:
+    :return:
+    """
     path = []
-    curr = cur_node
+    curr = curr_node
     while curr.get_parent():
         path.append(curr.get_action())
         curr = curr.get_parent()
 
-    return path[::-1] + first_plan[cur_node.get_node()]
-
-
-def is_visited_by_plan(curr_get_node: frozenset):
-    return curr_get_node in first_plan
-
-
-def build_first_plan_dict(curr_node: PQItem):
-    first_plan[curr_node.get_node()] = curr_node.get_action()
+    return path[::-1]
 
 
 def sort_successors(successors):
@@ -114,11 +110,11 @@ def sort_successors(successors):
             stop.append(successor)
         else:
             print("##### in search forgot " + successor[1].name + " ###########")
-    ret.extend(stop)
-    ret.extend(jump)
-    ret.extend(move)
-    ret.extend(goto)
     ret.extend(pay)
+    ret.extend(goto)
+    ret.extend(move)
+    ret.extend(jump)
+    ret.extend(stop)
 
     return ret
 
@@ -132,24 +128,24 @@ def general_search(problem, fringe):
     :param fringe:
     :return:
     """
-    fringe.push(PQItem((problem.get_start_state(), [], 0, None)))  # curr_node, action, cost, parent
-    closed = set()
 
+    fringe.push(PQItem((problem.get_start_state(), None, 0, None)))  # curr_node, action, cost, parent
+    closed = set()
     while not fringe.isEmpty():
         i = 1
         curr = fringe.pop()
+
         if problem.is_goal_state(curr.get_node()):
-            build_first_plan_dict(curr)
-            return curr.get_action()
-        elif is_visited_by_plan(curr.get_node()):
-            return get_past_path(curr.get_action())
+            return get_path(curr)
+
         elif curr.get_node() not in closed:
             successors = problem.get_successors(curr.get_node())
+            # if i % 1 == 0:
+                # print("Num_successors = %d" % len(successors))
+            successors = sort_successors(successors)
             for i in range(len(successors)):
-                next_state, action, cost = successors[i]
-                fringe.push(PQItem((next_state,  curr.get_action()+ [action] , cost + curr.get_cost(), None)))
+                fringe.push(PQItem((successors[i][0], successors[i][1], successors[i][2] + curr.get_cost(), curr)))
             closed.add(curr.get_node())
-        i += 1
     return "failed"
 
 
