bfc44b5d04a9e789b7e9ea69c3759d97642c01c4
erez <erez.kaufman@mail.huji.ac.il>
2019-08-10 12:05:19 +0300

    changed search's fringe ordering

--- graphplan/search.py
+++ graphplan/search.py
@@ -76,23 +76,23 @@ class SearchProblem:
         util.raiseNotDefined()
 
 
-def get_path(curr_node):
-    """
-
-    :param curr_node:
-    :return:
-    """
-    path = []
-    curr = curr_node
-    first_path = False
-    if len(first_plan) == 0:
-        first_path = True
-    while curr.get_parent():
-        path.append(curr.get_action())
-        curr = curr.get_parent()
-        if first_path:
-            build_first_plan_dict(curr, path[::-1])
-    return path[::-1]
+# def get_path(curr_node):
+#     """
+#
+#     :param curr_node:
+#     :return:
+#     """
+#     path = []
+#     curr = curr_node
+#     first_path = False
+#     if len(first_plan) == 0:
+#         first_path = True
+#     while curr.get_parent():
+#         path.append(curr.get_action())
+#         curr = curr.get_parent()
+#         if first_path:
+#             build_first_plan_dict(curr, path[::-1])
+#     return path[::-1]
 
 
 def get_past_path(cur_node):
@@ -109,9 +109,9 @@ def is_visited_by_plan(curr_get_node: frozenset):
     return curr_get_node in first_plan
 
 
-def build_first_plan_dict(curr_node: PQItem, path):
-    first_plan[curr_node.get_node()] = path
 
+def build_first_plan_dict(curr_node: PQItem):
+    first_plan[curr_node.get_node()] = curr_node.get_action()
 
 def sort_successors(successors):
     ret = []
@@ -152,24 +152,23 @@ def general_search(problem, fringe):
     :return:
     """
 
-    fringe.push(PQItem((problem.get_start_state(), None, 0, None)))  # curr_node, action, cost, parent
+    fringe.push(PQItem((problem.get_start_state(), [], 0, None)))  # curr_node, action, cost, parent
     closed = set()
+
     while not fringe.isEmpty():
         i = 1
         curr = fringe.pop()
-
         if problem.is_goal_state(curr.get_node()):
-            return get_path(curr)
-
-        # elif is_visited_by_plan(curr.get_node()):
-        #     print("VISITED!")
-        #     return get_past_path(curr)
-
+            build_first_plan_dict(curr)
+            return curr.get_action()
+        elif is_visited_by_plan(curr.get_node()):
+            print("VISITED!")
+            return get_past_path(curr.get_action())
         elif curr.get_node() not in closed:
             successors = problem.get_successors(curr.get_node())
-            successors = sort_successors(successors)
             for i in range(len(successors)):
-                fringe.push(PQItem((successors[i][0], successors[i][1], successors[i][2] + curr.get_cost(), curr)))
+                next_state, action, cost = successors[i]
+                fringe.push(PQItem((next_state, curr.get_action() + [action], cost + curr.get_cost(), None)))
             closed.add(curr.get_node())
         i += 1
     return "failed"
