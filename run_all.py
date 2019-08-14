118a97e4eb942633be172f5f12e86bccaee59d0b
Nitzan Wagner <t-niwagn@microsoft.com>
2019-08-14 13:42:37 +0300

    Merge branch 'master' of https://github.com/amsoff/AI_Project_Package_Arrived
    # Conflicts:
    #	graphplan/planning_problem.py

--- run_all.py
+++ run_all.py
@@ -4,11 +4,11 @@ import os
 def run():
     for type in ["optimistic"]: #, "average"]:
         for heuristic in ["sum", "max"]:
-            for gen_flag in range(1, 4):
+            for gen_flag in range(1, 3):
                 gen = "gen_%d" % gen_flag
-                for amnt in range(0, 2201, 200):
+                for amnt in range(0, 1001, 200):
                     x = 0
-                    os.system("python3 run_script.py {} {} {} {}".format(amnt, type, heuristic, gen_flag))
+                    os.system("python run_script.py {} {} {} {}".format(amnt, type, heuristic, gen_flag))
                     print("###########################################################")
                     print("FINISHED ROUND %d: type       - %s\n"
                           "                   heuristic - %s\n"
