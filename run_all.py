1359307acf422395b417e1e0f6249978d5c035a2
Nitzan Wagner <t-niwagn@microsoft.com>
2019-08-17 12:28:20 +0300

    run_all

--- run_all.py
+++ run_all.py
@@ -2,11 +2,11 @@ import os
 
 
 def run():
-    for type in ["average"]: #, "average"]:
-        for heuristic in ["sum"]:
-            for gen_flag in range(1, 4):
+    for type in ["optimistic", "average"]: #, "average"]:
+        for heuristic in ["max"]:
+            for gen_flag in range(1, 2):
                 gen = "gen_%d" % gen_flag
-                for amnt in [1500,300]:
+                for amnt in range(0, 201, 200):
                     x = 0
                     os.system("python run_script.py {} {} {} {}".format(amnt, type, heuristic, gen_flag))
                     print("###########################################################")
